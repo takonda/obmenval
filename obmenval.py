a1feac64eae50459972783072f522d4dd7307d6d
tatanakondakova <tnea@mail.ru>
2024-09-11 14:00:58 +0800

    Вместо поля ввода вставлен выпадающий список combobox

--- obmenval.py
+++ obmenval.py
@@ -2,10 +2,10 @@ import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
-
+from tkinter import ttk
 
 def exchange():
-    code = entry.get()
+    code = combobox.get()
 
     if code:
         try:
@@ -27,10 +27,13 @@ window=Tk()
 window.title('Курсы обмена валют')
 window.geometry('360x180')
 
-Label(text='Введите код валюты').pack(padx=10, pady=10)
+Label(text='Выберите код валюты').pack(padx=10, pady=10)
+cur = ['RUB', 'EUR', 'GBP', 'JPY', 'CNY', 'KZT', 'UZS', 'CHF', 'AED', 'CAD']
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack(padx=10, pady=10)
+# entry = Entry()
+# entry.pack(padx=10, pady=10)
 
 Button(text='Получить курс обмена к доллару', command=exchange).pack(padx=10, pady=10)
 
